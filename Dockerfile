9a2496518868a859a6feb9be2f1be5095e586727
K-Soo <kanabun102@naver.com>
2024-11-24 19:37:48 +0900

    init commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:20.18.0-alpine
+
+WORKDIR /app
+
+RUN corepack enable && corepack prepare pnpm@latest --activate
+
+COPY pnpm-lock.yaml ./
+
+COPY package.json ./
+
+RUN pnpm install
+
+COPY . /app
+
+EXPOSE 8080
+
+CMD ["npm", "run", "start:dev"]
---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TestsModule } from './tests/tests.module';
+
+@Module({
+  imports: [TestsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
